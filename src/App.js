b9f4cc4cdc562189722ecf36271be35b5de5ed39
rishab-sudo <rishabdutt4@gmail.com>
2026-05-29 13:31:43 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import './App.css';
 
 import HomeBanner from "./components/Home";
